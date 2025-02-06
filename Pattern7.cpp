ef70f0b3e6a05efe01e5272f01bb8885af2a319a
Aditya J Parida <126658487+adigithu@users.noreply.github.com>
2025-02-06 11:59:57 +0530

    Update Pattern7.cpp

--- Pattern7.cpp
+++ Pattern7.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
+int main(){
     int n;
     cout<<"Enter the value of n:";
     cin>>n;
-    for(int i = 0; i < n; i++){
+    for(int i=0;i<n;i++){
         for(int j=0;j<n-i-1;j++){
-            cout << " ";
+            cout<<" ";
         }
         for(int j=0;j<2*i+1;j++){
-            cout << "*";
+            cout<<"*";
         }
         cout<<endl;
     }
