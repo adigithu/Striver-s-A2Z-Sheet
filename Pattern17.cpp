3ca58c0a12cdfb9a7d45579c280f61f107f57d7e
Aditya J Parida <adityaparida605@gmail.com>
2025-02-07 19:10:22 +0530

    17th Pattern Solved

---
+++ Pattern17.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the value of n:";
+    cin>>n;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n - i - 1; j++){
+            cout << " ";
+        }
+        char ch = 'A';
+        for(int j = 0; j <= i; j++){
+            cout << ch;
+            ch++;
+        }
+        ch -= 2;
+        for(int j = 0; j < i; j++){
+            cout << ch;
+            ch--;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
